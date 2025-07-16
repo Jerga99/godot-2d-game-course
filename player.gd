779bf303eb17e163477342ff0f24f31439a3e3a2
Filip Jerga <jerga99@gmail.com>
2025-07-16 08:41:11 +0200

    Change player position

--- player.gd
+++ player.gd
@@ -2,5 +2,11 @@ extends Node2D
 
 
 func _process(delta: float):
-	print(delta)
+	
+	self.position += Vector2(20, 10) * delta
+	
+	self.position.x += 20 * delta
+	self.rotation += 10 * delta
+	self.scale += Vector2(0.4, 0.4) * delta
+	
 	
