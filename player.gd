2cc524da179641424f8b0f95da7b11b5008d1c82
Filip Jerga <jerga99@gmail.com>
2025-07-17 08:33:48 +0200

    Run animation

--- player.gd
+++ player.gd
@@ -2,12 +2,12 @@ extends Node2D
 
 @export var speed: float = 20
 
-@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+var is_moving: bool = false
 
-#func _ready():
-	#animated_sprite = get_node("AnimatedSprite2D")
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 	
 func _process(delta: float):
+	is_moving = false
 	var horizontal = Input.get_axis("left", "right")
 	var vertical = Input.get_axis("up", "down")
 	
@@ -16,7 +16,29 @@ func _process(delta: float):
 	self.position += n_movement * speed * delta
 	
 	if n_movement.length() > 0:
+		is_moving = true
 		if horizontal > 0:
 			animated_sprite.flip_h = false
 		elif horizontal < 0:
 			animated_sprite.flip_h = true
+			
+	_handle_animation()
+			
+func _handle_animation():
+	if is_moving:
+		animated_sprite.play("run")
+	else:
+		animated_sprite.play("idle")
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
