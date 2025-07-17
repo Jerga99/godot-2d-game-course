81677310c0ef96dc85570fe04e24c024c4af9cdb
Filip Jerga <jerga99@gmail.com>
2025-07-17 08:22:23 +0200

    Flip Animated Sprite

--- player.gd
+++ player.gd
@@ -2,10 +2,21 @@ extends Node2D
 
 @export var speed: float = 20
 
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+#func _ready():
+	#animated_sprite = get_node("AnimatedSprite2D")
+	
 func _process(delta: float):
 	var horizontal = Input.get_axis("left", "right")
 	var vertical = Input.get_axis("up", "down")
 	
 	var movement = Vector2(horizontal, vertical)
+	var n_movement = movement.normalized()
+	self.position += n_movement * speed * delta
 	
-	self.position += movement.normalized() * speed * delta
+	if n_movement.length() > 0:
+		if horizontal > 0:
+			animated_sprite.flip_h = false
+		elif horizontal < 0:
+			animated_sprite.flip_h = true
