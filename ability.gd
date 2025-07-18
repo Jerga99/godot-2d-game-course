7d9f04328e7f3d19df94e49b69edf4a1c887a127
Filip Jerga <jerga99@gmail.com>
2025-07-18 13:50:02 +0200

    Ability Components

--- ability.gd
+++ ability.gd
@@ -3,3 +3,9 @@ extends Node
 
 func activate():
 	print("Activating ability: ", self.name)
+	_activate_components()
+
+func _activate_components():
+	for child in get_children():
+		if child is AbilityComponent:
+			child.activate()
---
+++ ability_component.gd
@@ -0,0 +1,5 @@
+class_name AbilityComponent
+extends Node
+
+func activate():
+	print("Activation component: ", self.name)
