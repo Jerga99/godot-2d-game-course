e747230b499eb31cdef0a259b0afd42ad00c104f
Filip Jerga <jerga99@gmail.com>
2025-07-21 08:47:45 +0200

    Spawn object ability component

--- ability_component.gd
+++ ability_component.gd
@@ -3,3 +3,7 @@ extends Node
 
 func activate():
 	print("Activation component: ", self.name)
+	_activate()
+	
+func _activate():
+	pass
---
+++ spawn_object_ac.gd
@@ -0,0 +1,6 @@
+class_name SpawnObjectAc
+extends AbilityComponent
+
+
+func _activate():
+	print("Spawn Object Custom Functionality")
