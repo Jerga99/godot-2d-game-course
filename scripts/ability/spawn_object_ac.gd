eb2a5e1f11ec320d578cfe679d05184a9714d1a5
Filip Jerga <jerga99@gmail.com>
2025-07-21 09:49:23 +0200

    Add effect in the tree

--- scripts/ability/spawn_object_ac.gd
+++ scripts/ability/spawn_object_ac.gd
@@ -1,6 +1,12 @@
 class_name SpawnObjectAc
 extends AbilityComponent
 
+@export var object_scene: PackedScene
 
 func _activate():
-	print("Spawn Object Custom Functionality")
+	if object_scene == null: return
+	
+	var node = object_scene.instantiate()
+	var play_scene = get_tree().get_root().get_node("PlayScene")
+	play_scene.add_child((node))
+	
