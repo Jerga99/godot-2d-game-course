43ccf74a7a73543e5e8f9a342bef449cac00436e
Filip Jerga <jerga99@gmail.com>
2025-07-21 10:12:16 +0200

    Spawn node as child

--- scripts/ability/spawn_object_ac.gd
+++ scripts/ability/spawn_object_ac.gd
@@ -2,10 +2,19 @@ class_name SpawnObjectAc
 extends AbilityComponent
 
 @export var object_scene: PackedScene
+@export var set_as_child: bool = false
 
 func _activate(entity: Entity):
 	if object_scene == null: return
 	
 	var node = object_scene.instantiate()
-	entity.add_child((node))
+	
+	if set_as_child:
+		entity.add_child((node))
+	else:
+		var root = get_tree().get_root()
+		node.position = entity.position
+		root.add_child(node)
+		
+	
 	
