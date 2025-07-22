359a5279a3f910ef9272a66bfd86c4cafddf30a6
Filip Jerga <jerga99@gmail.com>
2025-07-22 08:48:12 +0200

    Spawn offset

--- scripts/ability/spawn_object_ac.gd
+++ scripts/ability/spawn_object_ac.gd
@@ -3,11 +3,12 @@ extends AbilityComponent
 
 @export var object_scene: PackedScene
 @export var set_as_child: bool = false
+@export var spawn_offset: Vector2 = Vector2.ZERO
 
 func _activate(entity: Entity):
 	if object_scene == null: return
 	
-	var node = object_scene.instantiate()
+	var node = object_scene.instantiate() as Node2D
 	
 	if set_as_child:
 		entity.add_child((node))
@@ -16,5 +17,7 @@ func _activate(entity: Entity):
 		node.position = entity.position
 		root.add_child(node)
 		
+	node.position += spawn_offset
+			
 	
 	
