7381d91481e31c011c0e8e2246f699618dce2421
Filip Jerga <jerga99@gmail.com>
2025-07-23 09:19:24 +0200

    Alternate slash

--- scripts/ability/manifest/slash_manifest.gd
+++ scripts/ability/manifest/slash_manifest.gd
@@ -3,10 +3,16 @@ extends AbilityManifest
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
+static var alternate_slash: bool = true
+
 func _activate(entity: Entity):
 	var mouse_pos = get_viewport().get_camera_2d().get_global_mouse_position()
 	look_at(mouse_pos)
+	
+	alternate_slash = not alternate_slash
+	animated_sprite.flip_v = alternate_slash
 
 func _process(delta):
 	if animated_sprite.frame_progress >= 1.0:
 		queue_free()
+		
