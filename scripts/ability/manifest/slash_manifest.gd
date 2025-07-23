6cd73a087cb266d116b244b09286b1de2db874a4
Filip Jerga <jerga99@gmail.com>
2025-07-23 09:11:08 +0200

    Cleanup slash effect

--- scripts/ability/manifest/slash_manifest.gd
+++ scripts/ability/manifest/slash_manifest.gd
@@ -1,6 +1,12 @@
 class_name SlashManifest
 extends AbilityManifest
 
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
 func _activate(entity: Entity):
 	var mouse_pos = get_viewport().get_camera_2d().get_global_mouse_position()
 	look_at(mouse_pos)
+
+func _process(delta):
+	if animated_sprite.frame_progress >= 1.0:
+		queue_free()
