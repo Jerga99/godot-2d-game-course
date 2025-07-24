5c54098b68f0bfff355641a989619594243e42e2
Filip Jerga <jerga99@gmail.com>
2025-07-24 08:23:52 +0200

    Mirror weapon position

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,11 +1,18 @@
 extends Entity
 
 @export var speed: float = 20
+@export var weapon: Node2D
 
 var is_moving: bool = false
+var weapon_right: Vector2
+var weapon_left: Vector2
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var ability_controller: AbilityController = $AbilityController
+
+func _ready():
+	weapon_right = weapon.position
+	weapon_left = self.position + (self.position - weapon.position)
 	
 func _process(delta: float):
 	_handle_movemment(delta)
@@ -33,8 +40,12 @@ func _handle_movemment(delta: float):
 		is_moving = true
 		if horizontal > 0:
 			animated_sprite.flip_h = false
+			weapon.position = weapon_right
+			weapon.rotation = deg_to_rad(35)
 		elif horizontal < 0:
 			animated_sprite.flip_h = true
+			weapon.position = weapon_left
+			weapon.rotation = deg_to_rad(-35)
 			
 func _handle_animation():
 	if is_moving:
