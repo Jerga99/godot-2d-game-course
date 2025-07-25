13a384bb06089612fddb4261ef990427b276c991
Filip Jerga <jerga99@gmail.com>
2025-07-25 09:59:11 +0200

    Clone weapon

--- scripts/ability/manifest/slash_manifest.gd
+++ scripts/ability/manifest/slash_manifest.gd
@@ -1,11 +1,12 @@
 class_name SlashManifest
 extends AbilityManifest
 
+static var alternate_slash: bool = true
 @export var rotation_offset: float = 45.0
 
-@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+var cloned_weapon: Node2D
 
-static var alternate_slash: bool = true
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 func _activate(context: AbilityContext):
 	var mouse_pos = get_viewport().get_camera_2d().get_global_mouse_position()
@@ -17,21 +18,34 @@ func _activate(context: AbilityContext):
 	var weapon = context.caster.get_node("Weapon") as Node2D
 	
 	if weapon != null:
+		weapon.hide()
 		var base_angle = (mouse_pos - context.caster.global_position).angle()
 		var offset_rad = deg_to_rad(rotation_offset)
 		
-		if alternate_slash:
+		if !alternate_slash:
 			offset_rad = -offset_rad
 		
 		var weapon_angle = base_angle + offset_rad
 		var weapon_direction = Vector2(cos(weapon_angle), sin(weapon_angle))
 		
-		weapon.global_position  = context.caster.global_position + weapon_direction * 15.0
-		weapon.rotation = weapon_angle + PI / 2
+		cloned_weapon = weapon.duplicate()
+		cloned_weapon.show()
+		context.caster.add_child((cloned_weapon))
+		
+		cloned_weapon.global_position  = context.caster.global_position + weapon_direction * 15.0
+		cloned_weapon.rotation = weapon_angle + PI / 2
 			
 func _process(delta):
 	if animated_sprite.frame_progress >= 1.0:
-		queue_free()
+		_finish_attack()
+		
+func _finish_attack():
+	hide()
+	if cloned_weapon:
+		await get_tree().create_timer(0.25).timeout
+		cloned_weapon.queue_free()
+	
+	queue_free()
 		
 		
 		
