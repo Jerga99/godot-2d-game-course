63672fcbdda63faf5d27dd0051d5c62c2306494e
Filip Jerga <jerga99@gmail.com>
2025-07-29 09:33:41 +0200

    Move to player loc

--- scripts/entity/enemy.gd
+++ scripts/entity/enemy.gd
@@ -3,7 +3,15 @@ extends Entity
 
 var player: Player
 
+@export var speed: float = 10.0
+
 func _ready():
 	super._ready()
 	player =  get_tree().get_first_node_in_group("player")
-	print(player.name)
+	
+	
+func _process(delta: float):
+	if player != null:
+		var dir = (player.position - self.position).normalized()
+		self.position += dir * speed * delta
+		
