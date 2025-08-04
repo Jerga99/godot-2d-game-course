691057a6596fd71c3d331320041817201c94a2a8
Filip Jerga <jerga99@gmail.com>
2025-08-04 10:32:04 +0200

    Delay deal damage

--- scripts/ability/core/ability_component.gd
+++ scripts/ability/core/ability_component.gd
@@ -1,8 +1,13 @@
 class_name AbilityComponent
 extends Node
 
+@export var exec_delay: float = 0.0
+
 func activate(context: AbilityContext):
-	print("Activation component: ", self.name)
+	
+	if exec_delay > 0:
+		await get_tree().create_timer(exec_delay).timeout
+	
 	_activate(context)
 	
 func _activate(context: AbilityContext):
