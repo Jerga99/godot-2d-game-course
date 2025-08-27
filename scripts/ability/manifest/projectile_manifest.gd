6521e0c05fef4e65797c95cc7bfbe7bdffe08667
Filip Jerga <jerga99@gmail.com>
2025-08-27 10:49:09 +0200

    Projectile movement

--- scripts/ability/manifest/projectile_manifest.gd
+++ scripts/ability/manifest/projectile_manifest.gd
@@ -2,5 +2,15 @@
 class_name ProjectileManifest
 extends AbilityManifest
 
+@export var speed = 10.0
+
+var current_dir = Vector2.ZERO
+
 func activate(context: AbilityContext):
-	print("Fireball Go!")
+	if context.targets.size() > 0:
+		var target_pos = context.get_target_position(0)
+		current_dir = (target_pos - global_position).normalized()
+		look_at(target_pos)
+
+func _process(delta):
+	global_position += current_dir * delta * speed
