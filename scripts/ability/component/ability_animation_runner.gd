ee2fd325075b2222ae54fab08d4b73b3213efa2e
Filip Jerga <jerga99@gmail.com>
2025-08-28 09:51:43 +0200

    Play cast animation

--- scripts/ability/component/ability_animation_runner.gd
+++ scripts/ability/component/ability_animation_runner.gd
@@ -1,5 +1,7 @@
 class_name AbilityAnimationRunner
 extends AbilityComponent
 
+@export var animation_name: String
+
 func _activate(context: AbilityContext):
-	context.caster.play_animation(AnimationWrapper.new("slash", true))
+	context.caster.play_animation(AnimationWrapper.new(animation_name, true))
