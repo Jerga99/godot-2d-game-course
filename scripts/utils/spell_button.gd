a9d455a5636f330a963fb5109e1c4998ee1cec41
Filip Jerga <jerga99@gmail.com>
2025-09-02 08:25:10 +0200

    Display progress visualy

--- scripts/utils/spell_button.gd
+++ scripts/utils/spell_button.gd
@@ -6,6 +6,7 @@ var ability: Ability = null
 
 @export var icon: TextureRect
 @export var progress_bar: TextureProgressBar
+@export var timer: Timer
 
 var binded_key: String = "":
 	set (key):
@@ -15,13 +16,26 @@ var binded_key: String = "":
 		input_key.keycode = key.unicode_at(0)
 		shortcut.events = [input_key]
 		
+func _process(delta):
+	if not timer.is_stopped():
+		progress_bar.value = timer.time_left
+		
 func set_ability(ability: Ability):
+	disabled = false
 	self.ability = ability
 	icon.texture = ability.icon_texture
+	
+	timer.wait_time = ability.cooldown
+	progress_bar.max_value = ability.cooldown
 
 func _on_pressed():
 	if ability == null: return
+	if disabled: return
 	
+	timer.start()
+	disabled = true
 	EventBus.play_cast_ability.emit(ability)
 	
-	
+func _on_timer_timeout():
+	timer.stop()
+	disabled = false
