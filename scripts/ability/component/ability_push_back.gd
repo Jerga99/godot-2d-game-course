ba4dca218c5d7c540504ba094591a97e76215ad4
Filip Jerga <jerga99@gmail.com>
2025-09-03 08:57:15 +0200

    Dash ability

--- scripts/ability/component/ability_push_back.gd
+++ scripts/ability/component/ability_push_back.gd
@@ -4,22 +4,29 @@ extends AbilityComponent
 @export var push_back_distance = 30
 @export var duration = 1.0
 @export var frequency = 3
+@export var revert = false
 
 var push_back_counter = 0
 var last_activation_time = -1
 
 func _activate(context: AbilityContext):
-	if Time.get_ticks_msec() - last_activation_time > 1000:
+	if frequency != -1 and Time.get_ticks_msec() - last_activation_time > 1000:
 		push_back_counter = 0
 	
 	push_back_counter += 1
-	if push_back_counter == frequency:
+	if frequency == -1 or push_back_counter == frequency:
 		push_back_counter = 0
 		var caster = context.caster
 		var caster_pos = caster.position
 		var mouse_pos = get_window().get_camera_2d().get_global_mouse_position()
 		
-		var push_dir = (caster_pos - mouse_pos).normalized()
+		var push_dir
+		
+		if revert:
+			push_dir = (mouse_pos - caster_pos).normalized()
+		else:
+			push_dir = (caster_pos - mouse_pos).normalized()
+		
 		var target_pos = caster_pos + push_dir * push_back_distance
 		
 		var tween = create_tween()
