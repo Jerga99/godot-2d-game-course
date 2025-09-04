188d396e5733608ce270a675e0090135d53217fd
Filip Jerga <jerga99@gmail.com>
2025-09-04 16:34:08 +0200

    Go to play scene

--- scripts/autoload/resource_locator.gd
+++ scripts/autoload/resource_locator.gd
@@ -2,6 +2,9 @@ extends Node
 
 var _play_scene: PlayScene = null
 
+var packed_play_scene: PackedScene = preload("res://scenes/play_scene.tscn")
+var packed_home_scene: PackedScene = preload("res://scenes/home_scene.tscn")
+
 func _ready():
 	_play_scene = get_tree().root.get_node("PlayScene")
 
@@ -10,3 +13,9 @@ func get_play_scene() -> PlayScene:
 		_play_scene = get_tree().root.get_node("PlayScene")
 		
 	return _play_scene
+
+func go_to_play_scene():
+	get_tree().change_scene_to_packed(packed_play_scene)
+	
+func go_to_home_scene():
+	get_tree().change_scene_to_packed(packed_home_scene)
---
+++ scripts/utils/home_scene.gd
@@ -0,0 +1,4 @@
+extends Node
+
+func _on_play_btn_pressed():
+	ResourceLocator.go_to_play_scene()
